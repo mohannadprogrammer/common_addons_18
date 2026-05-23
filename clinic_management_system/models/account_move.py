4d0de204dfa10be66dc88e64cb873306302571d1
mohannad waheed <mohannad.4.development@gmail.com>
2026-05-23 02:57:07 +0200

    remomve account move

--- clinic_management_system/models/account_move.py
+++ clinic_management_system/models/account_move.py
@@ -11,14 +11,3 @@ class AccountMove(models.Model):
     shift_id = fields.Many2one("clinic.shift", string="Shift")
     shift_state = fields.Selection(related='shift_id.state', string='Shift State', readonly=True)
 
-    def write(self, vals):
-        for rec in self:
-            if rec.shift_id and rec.shift_id.state == 'closed':
-                raise UserError(_('Cannot modify an invoice linked to a closed shift.'))
-        return super().write(vals)
-
-    def unlink(self):
-        for rec in self:
-            if rec.shift_id and rec.shift_id.state == 'closed':
-                raise UserError(_('Cannot delete an invoice linked to a closed shift.'))
-        return super().unlink()
